7692ea2734d3547fe2cb9009fefbea7bd54cc19e
Kaustubh <ksd.tcet@gmail.com>
2025-01-22 11:35:23 +0530

    updatd navgraph

--- app/src/main/java/com/example/screennavigation/App.kt
+++ app/src/main/java/com/example/screennavigation/App.kt
@@ -2,11 +2,14 @@ package com.example.screennavigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.createGraph
 import com.example.screennavigation.Screens.ScreenOne
 import com.example.screennavigation.Screens.ScreenThree
 import com.example.screennavigation.Screens.ScreenTwo
@@ -33,10 +36,28 @@ fun NavGraphBuilder.navigationGraph(navController: NavController) {
 
 @Composable
 fun App() {
-    // Nav Controller
+//    // Nav Controller
+//    val navController = rememberNavController()
+//    // Nav Host
+//    NavHost(navController = navController, startDestination = "screen-one") {
+//        navigationGraph(navController = navController)
+//    }
+    // Alternate way
     val navController = rememberNavController()
-    // Nav Host
-    NavHost(navController = navController, startDestination = "screen-one") {
-        navigationGraph(navController = navController)
+    NavHost(navController = navController, graph = getMyNavGraph(navController))
+}
+
+// another way to do Nav Graph
+fun getMyNavGraph(controller: NavController): NavGraph {
+    return controller.createGraph(startDestination = "screen-one") {
+        composable("screen-one") {
+            ScreenOne(navController = controller)
+        }
+        composable("screen-two") {
+            ScreenTwo(navController = controller)
+        }
+        composable("screen-three") {
+            ScreenThree(navController = controller)
+        }
     }
 }
